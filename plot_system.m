023c8c7c359e4490ff1a40e18a5c96748fd2253e
AriB2003 <ari.bobesh@gmail.com>
2025-11-10 21:02:58 -0500

    Add linearization and modal analysis

--- plot_system.m
+++ plot_system.m
@@ -11,20 +11,24 @@ function plot_system(t_list, V_list, box_params)
     for i=1:length(box_params.k_list)
         springs = [springs, initialize_spring_plot(num_zigs,w)];
     end
+    txt = text(-9,9,string(t_list(1)));
     axis equal; axis square;
     axis([-10,10,-10,10]);
     t_diff = diff(t_list);
-    for i=1:length(t_diff)
+    for j=1:length(t_diff)
+        tic()
         P1 = box_params.P_world;
-        P2 = compute_rbt(V_list(i,1),V_list(i,2),V_list(i,3),box_params.P_box);
-        P3 = compute_rbt(V_list(i,1),V_list(i,2),V_list(i,3),box_params.boundary_pts);
+        P2 = compute_rbt(V_list(j,1),V_list(j,2),V_list(j,3),box_params.P_box);
+        P3 = compute_rbt(V_list(j,1),V_list(j,2),V_list(j,3),box_params.boundary_pts);
         for i=1:length(sides)
             set(sides(i),'xdata',[P3(1,i),P3(1,mod(i,length(sides))+1)],'ydata',[P3(2,i),P3(2,mod(i,length(sides))+1)]);
         end
         for i=1:length(springs)
             update_spring_plot(springs(i),P1(:,i),P2(:,i));
         end
+        set(txt, 'string',string(t_list(j+1)))
         drawnow;
-        pause(t_diff(i));
+        p=toc();
+        pause(max(0,t_diff(j)-p));
     end
 end
--- simulate_box.m
+++ simulate_box.m
@@ -1,4 +1,5 @@
 function simulate_box()
+close all
 %define system parameters
 LW = 10; LH = 1; LG = 3;
 m = 1; Ic = (1/12)*(LH^2+LW^2);
@@ -27,7 +28,9 @@ box_params.P_box = P_box;
 box_params.boundary_pts = boundary_pts;
 %load the system parameters into the rate function
 %via an anonymous function
-my_rate_func = @(t_in,V_in) box_rate_func(t_in,V_in,box_params);
+my_rate_func2 = @(V_in) box_rate_func(0,V_in,box_params);
+Veq = multi_newton_solver(my_rate_func2, zeros(6,1), struct());
+
 x0 = 1;
 y0 = 1;
 theta0 = 1;
@@ -35,10 +38,57 @@ vx0 = 0;
 vy0 = 0;
 vtheta0 = 0;
 V0 = [x0;y0;theta0;vx0;vy0;vtheta0];
-tspan = [0,5];
+tspan = [0,10];
+
+epsilon = 10^-1;
+J_approx = approximate_jacobian(my_rate_func2, Veq);
+
+[U_mode, omega_n] = eigs(J_approx(4:6,1:3),3);
+omega_n = sqrt(-omega_n);
+hz = omega_n(3,3)/(2*pi)
+Vpert = Veq + epsilon*[U_mode(:,3);0;0;0];
+
+my_linear_rate = @(t_in,V_in) J_approx*(V_in-Veq);
+% Vpert = Veq + epsilon*V0;
+[t_list_lin,V_list_lin,~, ~, ~] = explicit_RK_variable_step_integration(my_linear_rate, tspan, Vpert, 0.01, rk_method("dormandprince"), 4, 10^-8);
+
 %run the integration
-[t_list,V_list,~, ~, ~] = explicit_RK_variable_step_integration(my_rate_func, tspan, V0, 0.01, rk_method("dormandprince"), 4, 10^-8);
-plot(t_list, V_list(:,1:3))
-legend({'x', 'y', 'theta'});
+my_rate_func = @(t_in,V_in) box_rate_func(t_in,V_in,box_params);
+[t_list,V_list,~, ~, ~] = explicit_RK_variable_step_integration(my_rate_func, tspan, Vpert, 0.01, rk_method("dormandprince"), 4, 10^-8);
+
+subplot(3,1,1);
+plot(t_list_lin, V_list_lin(:,1),".",DisplayName="Linearized", MarkerSize=3)
+hold on
+subplot(3,1,2);
+plot(t_list_lin, V_list_lin(:,2),".",DisplayName="Linearized", MarkerSize=3)
+hold on
+subplot(3,1,3);
+plot(t_list_lin, V_list_lin(:,3),".",DisplayName="Linearized", MarkerSize=3)
+hold on
+
+subplot(3,1,1);
+plot(t_list, V_list(:,1),"--",DisplayName="Non-Linearized")
+subplot(3,1,2);
+plot(t_list, V_list(:,2),"--",DisplayName="Non-Linearized")
+subplot(3,1,3);
+plot(t_list, V_list(:,3),"--",DisplayName="Non-Linearized")
+
+subplot(3,1,1);
+x_modal = Veq(1)+epsilon*U_mode(1,3)*cos(omega_n(3,3)*t_list);
+plot(t_list, x_modal,"-",DisplayName="Modal")
+ylabel("X");
+legend();
+subplot(3,1,2);
+y_modal = Veq(2)+epsilon*U_mode(2,3)*cos(omega_n(3,3)*t_list);
+plot(t_list, y_modal,"-",DisplayName="Modal")
+ylabel("Y");
+legend();
+subplot(3,1,3);
+theta_modal = Veq(3)+epsilon*U_mode(3,3)*cos(omega_n(3,3)*t_list);
+plot(t_list, theta_modal,"-",DisplayName="Modal")
+xlabel("Time (s)")
+ylabel("Theta");
+legend();
+
 plot_system(t_list,V_list,box_params)
 end
